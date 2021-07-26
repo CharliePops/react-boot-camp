c05d56096600b8b241989457062041a6408d84a2
Vitor Dino <me@vitordino.com>
2021-07-26 12:52:12 -0300

    :lipstick: use component styles on demo

--- components/08-ClassState/01-component.js
+++ components/08-ClassState/01-component.js
@@ -24,9 +24,11 @@ const instance = new ClassWithState(10)
 const App = () => {
   return (
     <div>
-      <pre>state: {instance.state}</pre>
-      <button onClick={instance.decrement}>decrement</button>
-      <button onClick={instance.increment}>increment</button>
+      <h1>Current count {instance.state}</h1>
+      <p>
+        <button onClick={instance.decrement}>decrement</button>
+        <button onClick={instance.increment}>increment</button>
+      </p>
     </div>
   )
 }
--- components/08-ClassState/03-code.js
+++ components/08-ClassState/03-code.js
@@ -21,19 +21,19 @@ class ClassWithState {
 
 const instance = new ClassWithState(10)
 
-const Reader = () => <pre>state: {instance.state}</pre>
+const Reader = () => <h1>Current count {instance.state}</h1>
 const Clicker = () => (
-  <>
+  <p>
     <button onClick={instance.decrement}>decrement</button>
     <button onClick={instance.increment}>increment</button>
-  </>
+  </p>
 )
 
 const App = () => (
-  <>
+  <div>
     <Reader/>
     <Clicker/>
-  </>
+  </div>
 )
 
 export default App
